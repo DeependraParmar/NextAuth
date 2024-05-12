10d5a375979dd01ebe47fe96d0be253bed2ab560
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-05-12 15:44:37 +0530

    Protected routes with middlewares

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,30 @@ import authConfig from "./auth.config";
 import NextAuth from "next-auth";
 
 const { auth } = NextAuth(authConfig);
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from "@/routes";
 
 export default auth((req) => {
+    const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
-    console.log("Route is: ", req.nextUrl.pathname)
-    console.log("Is logged in: ", isLoggedIn);
+
+    const isAPIAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if(isAPIAuthRoute){
+        return null;
+    }
+    if(isAuthRoute){
+        if(isLoggedIn){
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return null;
+    }
+    if(!isLoggedIn && !isPublicRoute){
+        return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+
+    return null;
 }); 
 
 export const config = {
