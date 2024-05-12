b87e6cbc09674582240763f19f65261b0a18643c
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-05-12 14:56:01 +0530

    Prisma added, setup done and register done with password hashing

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { prisma } from "@/lib/prisma"; 
+import { getUserByEmail } from "@/data/user";
 
 export const register = async(values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = await RegisterSchema.safeParse(values);
@@ -9,5 +12,20 @@ export const register = async(values: z.infer<typeof RegisterSchema>) => {
         return { error: "Invalid Credentials !!" }
     }
 
-    return { success: "Email Sent" }
+    const { name, email, password } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingUser = await getUserByEmail(email);
+
+    if(existingUser)
+        return { error: 'Email already exists !'}
+
+    await prisma.user.create({
+        data: {
+            name, email, password: hashedPassword
+        }
+    });
+
+    // Todo: Send the verification mail here 
+
+    return { success: "Registered Successfully" }
 }
