7f2f33a2e19c7884c8a9c1b515d82ffece2ccfa2
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-05-12 13:34:45 +0530

    Added error and success message component

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -7,6 +7,8 @@ import * as z from "zod";
 import { LoginSchema } from '@/schemas'
 import { Input } from '@/components/ui/input';
 import { Button } from '../ui/button';
+import FormError from '../form-error';
+import FormSuccess from '../form-success';
 
 
 export const LoginForm = () => {
@@ -46,6 +48,8 @@ export const LoginForm = () => {
                             </FormItem>
                         )} />
                     </div>
+                    <FormError message='' />
+                    <FormSuccess message='' />
                     <Button typeof='submit' className='w-full'>Login</Button>
                 </form>
             </Form>
