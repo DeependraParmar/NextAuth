6e641b8189bdc60a141a06ec45e6927de947ec34
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-05-12 15:24:16 +0530

    Added edge compatability for prisma

--- auth.ts
+++ auth.ts
@@ -1,10 +1,13 @@
 import NextAuth from "next-auth"
-import Github from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./lib/prisma";
+import authConfig from "./auth.config";
 
 export const {
     handlers: { GET, POST},
     auth,
 } = NextAuth({
-    providers: [Google, Github],
+    adapter: PrismaAdapter(prisma),
+    session: { strategy: "jwt"},
+    ...authConfig,
 });
